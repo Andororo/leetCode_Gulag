f9795c8269d0cad8c0d560bf1e4a7c2015923646
Andororo <andororoo@gmail.com>
2023-12-05 19:11:58 -0800

    scrape data from each tab

--- webscraper/webscrape.js
+++ webscraper/webscrape.js
@@ -1,3 +1,4 @@
+const { doc } = require("prettier");
 const puppeteer = require("puppeteer");
 
 const url = "https://neetcode.io/practice/";
@@ -20,14 +21,13 @@ const initiatePuppeteer = async () => {
  * @param {*} page
  * @returns List of catorgries name and the count of the tabs
  */
-const openEachTabs = async (page) => {
+const scrapeEachCatorgries = async (page) => {
   //find the first intence of div class then return an array of elements
   const tabLinks = await page.$$(
     "div.tabs.is-centered.is-boxed.is-large ul.tabs-list li a.tab-link"
   );
-  let categories
-  for (let k = 1; k < 4; k++){
-    await tabLinks[k].click();
+  
+    await tabLinks[3].click();
     await page.waitForSelector("app-pattern-table");
     await page.waitForSelector(
       "button.flex-container-row.accordion.button.is-fullwidth.ng-tns-c41-1"
@@ -40,9 +40,8 @@ const openEachTabs = async (page) => {
       categories.map((category) => category.textContent)
     );
     //console.log("Array: ", categoryArray)
-    
     const categoryLength = categoryArray.length;
-    //console.log(categoryLength);
+    // console.log(categoryLength);
 
     //with known tabs, click and open each single one of them
     for (let i = 0; i < categoryLength; i++) {
@@ -51,34 +50,84 @@ const openEachTabs = async (page) => {
       const tabSelector = `button.flex-container-row.accordion.button.is-fullwidth.ng-tns-c41-${i}`;
       //wait for the element to appear on the page
       await page.waitForSelector(tabSelector);
+      await addDelay(100)
       // use a element handle to reference a DOM to call function on
       const tabsElementHandle = await page.$(tabSelector);
       await tabsElementHandle.click();
     }
-
     // Extract category name
-    const categories = await page.evaluate(() =>
-      Array.from(
-        document.querySelectorAll(
-          "button.flex-container-row.accordion.button.is-fullwidth"
-        ),
-        (e) => e.querySelector("p").innerText
-      )
-    );
-    console.log(categories);
-  }
-  return {categories};
+    // const categories = await page.evaluate(() =>
+    //   Array.from(
+    //     document.querySelectorAll(
+    //       "button.flex-container-row.accordion.button.is-fullwidth"
+    //     ),
+    //     (e) => e.querySelector("p").innerText
+    //   )
+    // );
+  //console.log(categories)
+  //return {categories};
 };
 
 //Scrape each problem title, the link, and difficulty 
+const scrapeFromTabs = async (page, index) => {
+  //find the first intence of div class then return an array of elements
+  const tabLinks = await page.$$(
+    "div.tabs.is-centered.is-boxed.is-large ul.tabs-list li a.tab-link"
+  );
+  await tabLinks[index].click();
+  
+  const problem = await page.$$eval('tr.ng-star-inserted', (rows) => {
+    return rows.map((row) => {
+      // Extracting category information
+      // return a reference to the found element
+      const categoryElement = row.closest(".accordion-container")?.querySelector('button.flex-container-row.accordion.button.is-fullwidth.active p');
+      const category = categoryElement ? categoryElement.textContent.trim() : null;
+      //get difficulties
+      const difficultyElement = row.querySelector('td.diff-col b')
+      const difficulty = difficultyElement ? difficultyElement.textContent.trim() : null
+      //get link
+      const linkElement = row.querySelector('td a.table-text.text-color')
+      const linkHref = linkElement ? linkElement.href : null
+      //get text
+      const text = linkElement ? linkElement.textContent.trim() : null
+      return {
+        text: text,
+        category: category,
+        difficulty: difficulty,
+        href: linkHref
+      }
+    }) 
+  })
+  return problem
+}
 
+const scrapeBlind75 = async (page) => {
+  return await scrapeFromTabs(page, 1)
+}
+const scrapeNeetCode150 = async (page) => {
+  return await scrapeFromTabs(page, 2)
+}
+const scrapeAllProblem = async (page) => {
+  return await scrapeFromTabs(page, 3)
+}
+
+/**
+ * A function that add the problem list into a json file
+ */
 
 
 
 (async () => {
   const { browser, page } = await initiatePuppeteer();
-  await openEachTabs(page);
+  await scrapeEachCatorgries(page);
+  const problem1 = await scrapeBlind75(page)
+  const problem2 = await scrapeNeetCode150(page)
+  const problem3 = await scrapeAllProblem(page)
+  //add to a json file
+  browser.close()
 })();
+
+
 
 /*
   Just in case the website changes, and the tabs are no longer in the same order, or there's a new tab, here's the code to check (hopefully it still works)
